9729a9ec149370f4856ec7806550fbf26744302d
sarasn93 <sara.naghipoor@yahoo.com>
2021-12-23 14:45:19 +0330

    application layer

--- src/Services/Ordering/Ordering.Application/Mappings/MappingProfile.cs
+++ src/Services/Ordering/Ordering.Application/Mappings/MappingProfile.cs
@@ -1,4 +1,8 @@
 ﻿using AutoMapper;
+using Ordering.Application.Features.Orders.Commands.CheckOutOrder;
+using Ordering.Application.Features.Orders.Commands.UpdateOrder;
+using Ordering.Application.Features.Orders.Queries.GetOrderList;
+using Ordering.Domain.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,5 +13,13 @@ namespace Ordering.Application.Mappings
 {
     public class MappingProfile: Profile
     {
+        public MappingProfile()
+        {
+            CreateMap<Order, OrdersVm>().ReverseMap();
+            CreateMap<Order, CheckoutOrderrCommand>().ReverseMap();
+            CreateMap<Order, UpdateOrderCommand>().ReverseMap();
+            CreateMap<object, object>().ReverseMap();
+        }
+        
     }
 }
